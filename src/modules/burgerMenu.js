d8caf2638a7b053f7f370957501cf4f33daeae11
Kirill Demidov <kirill.demidov.1995@mail.ru>
2021-03-22 18:35:54 +0300

    Фиксация БУРГЕР-меню при скролле

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,6 +1,46 @@
 const burgerMenu = () => {
-    const popupMenu = document.querySelector('.popup-menu');
+    const popupMenu = document.querySelector('.popup-menu'),
+        menuBtn = document.querySelector('.menu-button'),
+        menu = document.querySelector('.top-menu'),
+        head = document.querySelector('.head'),
+        headHeight = head.clientHeight,
+        menuBtnDisplay = window.getComputedStyle(menuBtn).display;
 
+//Фиксация БУРГЕР-меню при скролле
+    const fixedMenuFunc = () => {
+        const fixedMenu = () => {
+            if(window.scrollY > headHeight){
+                menu.classList.add('fixed-menu');
+            } else {
+                menu.classList.remove('fixed-menu');
+            }     
+        };
+    
+        window.onscroll = () => {
+            if(menuBtnDisplay === 'block'){
+                fixedMenu();
+            }
+        };
+    
+        window.addEventListener('resize', () => {
+            const menuBtnDisplay = window.getComputedStyle(menuBtn).display;
+    
+            if(menuBtnDisplay === 'block'){
+                window.onscroll = () => {
+                    fixedMenu();
+                };
+            } else {
+                window.onscroll = () => {
+                    menu.classList.remove('fixed-menu');
+                };
+            }
+        });
+    
+        fixedMenu();
+    };
+    fixedMenuFunc();
+    
+//Проверка ОТКРЫТО или ЗАКРЫТО меню
     const menuOpenClose = () => {
         if(popupMenu.style.display === 'block'){
             popupMenu.style.display = 'none';
