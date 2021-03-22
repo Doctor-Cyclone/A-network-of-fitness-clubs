660fe075aae006330f48e3a17fd7c9bcc4a40386
Kirill Demidov <kirill.demidov.1995@mail.ru>
2021-03-22 13:04:49 +0300

    Добавил модальное окно с ПОДАРКОМ

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,12 +1,17 @@
 const popup = () => {
     const freeVisitForm = document.getElementById('free_visit_form'),
-        callbackForm = document.getElementById('callback_form');
+        callbackForm = document.getElementById('callback_form'),
+        giftForm = document.getElementById('gift'),
+        fixedGift = document.querySelector('.fixed-gift');
 
     const closePopup = (target) => {
         if(target.closest('#free_visit_form')){
             freeVisitForm.style.display = 'none';
-        } else {
+        } else if(target.closest('#callback_form')) {
             callbackForm.style.display = 'none';
+        } else {
+            giftForm.style.display = 'none';
+            fixedGift.style.display = 'block';
         }
     };
 
@@ -17,9 +22,14 @@ const popup = () => {
             freeVisitForm.style.display = 'block';
         } else if(target.classList.contains('callback-btn')){
             callbackForm.style.display = 'block';
+        } else if(target.closest('.fixed-gift')){
+            fixedGift.style.display = 'none';
+            giftForm.style.display = 'block';
         } else if(target.closest('.close-form')){
             closePopup(target);
         } else if(target.closest('.overlay')){
+            closePopup(target);
+        } else if(target.closest('.close-btn')){
             closePopup(target);
         }
     });
