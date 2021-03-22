504aadd6416966200bf415b44eb4a24ab4ada9c0
Kirill Demidov <kirill.demidov.1995@mail.ru>
2021-03-22 13:48:10 +0300

    Готовый главный слайдер

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import clubChoice from './modules/clubChoice';
 import popup from './modules/popup';
+import mainSlider from './modules/mainSlider';
 
 clubChoice();
 popup();
+mainSlider();
---
+++ src/modules/mainSlider.js
@@ -0,0 +1,44 @@
+const mainSlider = () => {
+    const slider = document.querySelector('.main-slider'),
+        slideText = slider.querySelectorAll('.slide-text'),
+        slide = slider.querySelectorAll('.slide');
+
+    let currentSlide = 0, interval;
+
+    const prevSlide = (item, index) => {
+        item[index].style.display = 'none';
+        },
+        nextSlide = (item, index) => {
+            item[index].style.display = 'block';
+        },
+        autoPlaySlide = () => {
+            prevSlide(slide, currentSlide);
+
+            currentSlide++;
+
+            if(currentSlide >= slide.length){
+                currentSlide = 0;
+            }
+
+            nextSlide(slide, currentSlide);
+        },
+        startSlide = (time = 2000) => {
+            interval = setInterval(autoPlaySlide, time);
+        },
+        stopSlide = () => {
+            clearInterval(interval);
+        };
+    
+    slideText.forEach( item => {
+        item.addEventListener('mouseover', () => {
+            stopSlide();
+        });
+        item.addEventListener('mouseout', () => {
+            startSlide(2000);
+        }); 
+    });   
+
+    startSlide(2000);
+};
+
+export default mainSlider;
