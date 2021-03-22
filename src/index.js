a5779250c9c0b5f189a40b42bf815c72bde8c8cf
Kirill Demidov <kirill.demidov.1995@mail.ru>
2021-03-23 00:28:37 +0300

    Появление стрелки при скролле

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ const showArrowAfterScroll = () => {
 
     toTop.style.display = 'none';
 
+    toTop.addEventListener('click', () => {
+        window.scrollTo(0, 0);
+        toTop.style.display = 'none';
+    });
     window.onscroll = () => {
         if (document.documentElement.scrollTop >= 700){
             toTop.style.display = 'block';
